dd80d2e808d2b3625437ec81363ee1ce22e64c3d
Cady <40341@ma-web.nl>
2025-11-18 12:35:26 +0100

    Random

---
+++ 01_functions/03_readlines/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03_readlines;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+
+    internal void Vraag1()
+    {
+        Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+        string antwoord = Console.Readline();
+
+        Console.WriteLine(antwoord);
+    }
+
+    internal void Run()
+    {
+        Console.WriteLine("I can Survive atleast 2 hours");
+        Console.WriteLine(Vraag1);
+    }
+
+}
