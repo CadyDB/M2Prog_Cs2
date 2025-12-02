6183f4f45e3bac1b53eaeab8f5fed3d4ab771dfa
Cady <40341@ma-web.nl>
2025-12-02 12:10:59 +0100

    les 3 opdracht

--- 03_constructors/Constructor/Program.cs
+++ 03_constructors/Constructor/Program.cs
@@ -8,8 +8,23 @@ class Program
         program.Run();
     }
 
+    internal Quiz()
+    {
+        int aantalVragen;
+        aantalVragen = new QuizVraag[aantalVragen];
+        aantalVragen = new QuizVraagAntwoord[ingevuldeAntwoorden];
+    }
+
+    internal QuizVraagAntwoord(QuizVraag vraag)
+    {
+        vraag = vraag;
+        goed = false;
+    }
+
     private void Run()
     {
         QuizVraag quizVraag = new QuizVraag("hier komt de vraag", "hier komt het antwoord");
+        Quiz quiz = new Quiz(10);
     }
+
 }
