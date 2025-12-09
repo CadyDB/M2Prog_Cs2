2ca688020c94668a3dfceed656befb913056ab06
Cady <40341@ma-web.nl>
2025-12-09 12:47:49 +0100

    1e opdracht

--- 04_lists/ForHerhaling/Program.cs
+++ 04_lists/ForHerhaling/Program.cs
@@ -2,8 +2,40 @@
 
 class Program
 {
+        double[] prijzen = new double[] {0.99, 5.60, 10.11};
+
+        string [] artiekelen = {"snoepje", "luxe broodje", "lunch menu"};
+
+        Formulier[] formulieren = new Formulier[2];
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.Run();
+        
     }
+
+    internal void Run()
+    {
+    
+        formulieren[0] = new Formulier()
+        {
+            Feedback = "prijzig maar lekker",
+            Sterren = 5
+        };
+
+        formulieren[1] = new Formulier()
+        {
+            Feedback = "lunch was wel erg veel",
+            Sterren = 2
+        };
+
+        for (int i = 0; i < prijzen.Length; i++)
+        {
+            Console.WriteLine(prijzen[i]);
+            Console.WriteLine(artiekelen[i]);
+        }
+    }
+
+    
 }
---
+++ 04_lists/Lists/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program[];
+        program.Run();
+    }
+
+    internal void Run()
+    {
+        
+    }
+}
