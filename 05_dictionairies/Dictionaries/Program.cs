84c98de913a6fd2efd2a5537ab19915401826adf
Cady <40341@ma-web.nl>
2026-01-06 11:43:50 +0100

    opdracht

---
+++ 05_dictionairies/Dictionaries/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program[];
+        program.Run();
+    }
+
+    internal void Run()
+    {
+        Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>( 
+            personeelOpNummer.Add(382942, "jim"),
+            personeelOpNummer.Add(789432, "tim")
+        );
+
+        string jim = personeelOpNummer[382942];
+        string tim = personeelOpNummer[789432];
+
+        foreach(KeyValuePair<int, string> item in personeelOpNummer)
+        {
+            return personeelOpNummer;
+        }
+
+        foreach(int key in personeelOpNummer.Keys)
+        {
+            
+        }
+
+        foreach(string value in personeelOpNummer.Values)
+        {
+            
+        }
+    }
+}
+
