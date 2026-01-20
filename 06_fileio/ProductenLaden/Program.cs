1472477068bc83b7eb9e487f013cbc0b2d49d004
Cady <40341@ma-web.nl>
2026-01-20 12:31:28 +0100

    bijna klaar

---
+++ 06_fileio/ProductenLaden/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductenLaden;
+
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.RunProducten();
+    }
+
+    private void RunProducten()
+    {
+        string text = File.ReadAllText("Producten.json");
+        Product[] producten = JsonSerializer.Deserialize<Product[]>(text);
+
+        foreach (Product product in producten)
+        {
+            Console.WriteLine(product.Name);
+        Console.WriteLine(product.Description);
+        Console.WriteLine(product.Price);
+        }
+        
+    }
+
+
+    private void Run()
+    {
+        string text = File.ReadAllText("Product.json");
+        Product product = JsonSerializer.Deserialize<Product>(text);
+        Console.WriteLine(product.Name);
+        Console.WriteLine(product.Description);
+        Console.WriteLine(product.Price);
+    }
+}
