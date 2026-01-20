8b75b2a155722e1e93943c60e95652b9f48536b0
Cady <40341@ma-web.nl>
2026-01-20 12:04:14 +0100

    changes

--- 06_fileio/MemSave/Program.cs
+++ 06_fileio/MemSave/Program.cs
@@ -10,28 +10,21 @@ namespace MemSave;
 
 class Program
 {
+    string welkomsTekst = "Hello World";
     static void Main(string[] args)
     {
         Program program = new Program();
         program.Run;
     }
 
-    internal void Run()
+    private void Run()
     {
-        string welkomsTekst = new string["Hello World"];
         Console.WriteLine(welkomsTekst);
-
-        string[] saveFile = new string[welkomState.txt];
-        bool bestaatDeFile = File.Exists(saveFile);
-
-
+        
         while (true)
         {
-            Console.WriteLine(welkomstekst);
-            Console.WriteLine("Enter a text, then press enter");
-            Console.ReadLine();
-
-            File.WriteAllText(saveFile, welkomsTekst);
+            Console.WriteLine("enter a text, then press enter");
+            Console.ReadLine();        
         }
     }
 }
